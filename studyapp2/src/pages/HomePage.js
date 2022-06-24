a0e0691f2c451e335888014da9dfc63184567cf2
yyeonzu <tooma22@naver.com>
2022-06-24 14:24:32 +0900

    feat: HOME PAGE 작성 완료
    1. map을 이용해 App.js의 posts를 렌더링
    2. post 꾸미기

--- studyapp2/src/pages/HomePage.js
+++ studyapp2/src/pages/HomePage.js
@@ -3,12 +3,21 @@ import styled from 'styled-components';
 
 const HomePage = ({ posts }) => {
 	return (
-		// map을 활용해 posts를 렌더링 하세요 
+		// map을 활용해 posts를 렌더링 하세요
+		<div>
+			{posts.map(post => (
+				<Text key={post.id}>
+					{post.title} : {post.content}
+				</Text>
+			))}
+		</div>
 	);
 };
 
 export default HomePage;
 
 const Text = styled.p`
-	// post를 꾸미세요 
+	// post를 꾸미세요
+	border: solid;
+	padding: 5px;
 `;
